574646f1d1ca1d9ef96e5e78860a66b6b831e464
suraj sundariya <surajs@exzeo.com>
2018-12-09 01:06:17 +0530

    import schema in index

---
+++ src/data/schema/index.js
@@ -0,0 +1,13 @@
+import * as User from './user';
+import * as Order from './order';
+import * as Product from './product';
+import * as Category from './category';
+import * as RootQuery from './root';
+
+export default {
+    RootQuery,
+    User,
+    Order,
+    Product,
+    Category
+};

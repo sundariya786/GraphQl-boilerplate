74350b204e19ff7eaecd80c5e69e344d0c9b1eba
suraj sundariya <surajs@exzeo.com>
2018-12-09 01:06:39 +0530

    model index

---
+++ src/models/index.js
@@ -0,0 +1,9 @@
+import { from } from 'apollo-link';
+
+export { default as User } from './users';
+
+export { default as  Order } from './order';
+
+export { default as Product } from './product';
+
+export { default as Category } from './category';

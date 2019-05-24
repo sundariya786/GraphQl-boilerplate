29c1b51f3043ad663e50b04cba6c3d16952e7e55
suraj sundariya <surajs@exzeo.com>
2019-05-25 02:54:56 +0530

    update  server  file

--- server.js
+++ server.js
@@ -1,99 +1,3 @@
-// import express from 'express';
-// import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-// import bodyParser from 'body-parser';
-// import schema from './src/schema';
-// const cors = require('cors')
-
-// import {User} from './src/models/users';
-
-// const GRAPHQL_PORT = 5000;
-// const MONGO_PORT = 27017;
-// const MONGO_URL = 'localhost';
-// const dbName = 'graphExample';
-
-// let mongoose = require('mongoose');
-// mongoose.Promise = global.Promise;
-// mongoose.connect(`mongodb://${MONGO_URL}:${MONGO_PORT}/${dbName}`, {
-//     useMongoClient: true
-// });
-
-// const graphQLServer = express();
-// graphQLServer.use(cors())
-
-// //graphQL
-// graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-// graphQLServer.use('/graphiql', graphiqlExpress({
-//   endpointURL: '/graphql',
-//   subscriptionsEndpoint: `ws://localhost:${GRAPHQL_PORT}/subscriptions`,
-// }));
-
-// //rest api instead
-// graphQLServer.get('/users', (req, res, next) => {
-//     User.find({}).exec((_err, _res) => res.json(_res));
-// });
-
-// graphQLServer.listen(GRAPHQL_PORT, () =>
-//   console.log(
-//     `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
-//   )
-// );
-
-
-// const { createServer } = require('http')
-// const express = require('express')
-// const bodyParser = require('body-parser')
-// const { graphqlExpress, graphiqlExpress } = require('graphql-server-express')
-// const { SubscriptionServer } = require('subscriptions-transport-ws')
-// const { subscribe, execute } = require('graphql')
-// import schema from './src/schema';
-// const db = require('./db');
-
-// const app = express()
-
-// const dev = process.env.NODE_ENV !== 'production'
-// const PORT = process.env.PORT || 5000
-
-
-// app.use(bodyParser.json())
-
-// app.use(
-//   '/graphql',
-//   graphqlExpress({
-//     context: {
-//       db
-//     },
-//     schema
-//   })
-// )
-
-// app.use(
-//   '/graphiql',
-//   graphiqlExpress({
-//     endpointURL: '/graphql',
-//     subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`
-//   })
-// )
-
-// const server = createServer(app)
-
-// server.listen(PORT, err => {
-//   if (err) throw err
-
-//   new SubscriptionServer(
-//     {
-//       schema,
-//       execute,
-//       subscribe,
-//       onConnect: () => console.log('Client connected')
-//     },
-//     {
-//       server,
-//       path: '/subscriptions'
-//     }
-//   )
-
-//   console.log(`> Ready on PORT ${PORT}`)
-// })
 
 
 
@@ -111,8 +15,6 @@ import { execute, subscribe } from 'graphql';
 import { createServer } from 'http';
 import { SubscriptionServer } from 'subscriptions-transport-ws';
 
-const PORT = 4000;
-const server = express();
 const MONGO_PORT = 27017;
 const MONGO_URL = 'localhost';
 const dbName = 'graphExample';
@@ -120,6 +22,9 @@ const dbName = 'graphExample';
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb://${MONGO_URL}:${MONGO_PORT}/${dbName}`);
+
+const PORT = 5000;
+const server = express();
 
 server.use('*', cors({ origin: 'http://localhost:3000' }));
 
@@ -148,4 +53,5 @@ ws.listen(PORT, () => {
     path: '/subscriptions',
   });
 });
+
 

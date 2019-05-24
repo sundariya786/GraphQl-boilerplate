0e27153163b8fabcb3944ce8ae17fa1d3f6bdd29
suraj sundariya <surajs@exzeo.com>
2019-05-25 02:56:24 +0530

    schema  for user

--- src/data/schema/user.js
+++ src/data/schema/user.js
@@ -5,19 +5,48 @@ export const schema = `
 
     # All user  information 
     type User {
-        # A unique key  for the user table 
+        # A unique key  for the userid  
         id: String
+        # game user  use  to play array  of  string with  game  code 
+        games: [String]
+        #followeres of the user "array  of user ids "
+        followers: [String]
+        createdAt: String
+        profile: Profile
+        disabled: Boolean
+        active: String
+        online: Boolean
+        services: Services
+    }
 
-        # name of the user
-        name:String
+    #profile info  of the  user 
+    type Profile {
+        firstName: String!
+        lastName: String
+        city:String
+        zip: String
+        mobileNumber: Int
+        games: [String]
+    }
 
-        # surname of the user
-        surname: String
+    type Services {
+        password: String!
+        token : String
     }
 `;
 
 export const resolvers = {
     User: {
         id: ({ _id }) => _id,
+        async profile({ profile }) {
+            return await db.Profile.findById({ _id: profile });
+        },
+        services: ({ services }) => services
+    },
+
+    Profile: {
+        firstName: ({ first_name }) => first_name,
+        lastName: ({ last_name }) => last_name,
+        mobileNumber: ({ mobile_num }) => mobile_num
     }
 }
